81c6af306d228cd3a1d31a0702654aabd6110fd3
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-02 15:45:54 -0700

    feat: Added add cpplint

--- utils_tool.cmake
+++ utils_tool.cmake
@@ -1,68 +1,50 @@
 macro(util_add_program_base program_name default_switch append_var)
-string(TOUPPER ${program_name} upper_program_name)
-option(USE_${upper_program_name} "run ${program_name}" ${default_switch})
-if(USE_${upper_program_name})
-    find_program(${append_var} NAMES ${program_name})
-    if(NOT ${append_var})
-        message(FATAL_ERROR "can't find ${program_name}")
-    endif()
+    string(TOUPPER ${program_name} upper_program_name)
+    option(USE_${upper_program_name} "run ${program_name}" ${default_switch})
+    if(USE_${upper_program_name})
+        find_program(${append_var} NAMES ${program_name})
+        if(NOT ${append_var})
+            message(FATAL_ERROR "can't find ${program_name}")
+        endif()
 
-    cmake_parse_arguments(PARSED "" "EXTRA_FLAG;OVERRIDE_FLAG" "" ${ARGN})
-    
-    if((PARSED_EXTRA_FLAG) AND (PARSED_OVERRIDE_FLAG))
-    message(FATAL_ERROR "EXTRA_FLAG and OVERRIDE_FLAG can't be defined at the same time")
-    endif()
+        cmake_parse_arguments(PARSED "" "EXTRA_FLAG;OVERRIDE_FLAG" "" ${ARGN})
+        
+        if((PARSED_EXTRA_FLAG) AND (PARSED_OVERRIDE_FLAG))
+        message(FATAL_ERROR "EXTRA_FLAG and OVERRIDE_FLAG can't be defined at the same time")
+        endif()
 
-    set(${program_name}_program_flag ${ARGN})
-    if(PARSED_EXTRA_FLAG)
-        string(APPEND ${program_name}_program_flag " " ${PARSED_EXTRA_FLAG})
-    endif()
-    if(PARSED_OVERRIDE_FLAG)
-        set(${program_name}_program_flag ${PARSED_OVERRIDE_FLAG})
-    endif()
+        set(${program_name}_program_flag ${ARGN})
+        if(PARSED_EXTRA_FLAG)
+            string(APPEND ${program_name}_program_flag " " ${PARSED_EXTRA_FLAG})
+        endif()
+        if(PARSED_OVERRIDE_FLAG)
+            set(${program_name}_program_flag ${PARSED_OVERRIDE_FLAG})
+        endif()
 
-    list(APPEND ${append_var} ${${program_name}_program_flag})
-    message("append var is ${${append_var}}")
-    message("program flag is ${${program_name}_program_flag}")
-endif()
+        list(APPEND ${append_var} ${${program_name}_program_flag})
+        message("append var is ${${append_var}}")
+        message("program flag is ${${program_name}_program_flag}")
+    endif()
 endmacro()
 
 # has 2 optional args
 # util_add_cppcheck EXTRA_FLAG extra_flag OVERRIDE_FLAG override_flag
 macro(util_add_cppcheck)
-util_add_program_base(cppcheck 
-                      OFF 
-                      CMAKE_CXX_CPPCHECK
-                      "--enable=all"
-                      "-q"
-                      "--force"
-                      "--suppressions-list=${util_cmake_dir}/cppcheck_suppression.txt")
+    util_add_program_base(cppcheck 
+                          OFF 
+                          CMAKE_CXX_CPPCHECK
+                          "--enable=all"
+                          "-q"
+                          "--force"
+                          "--suppressions-list=${util_cmake_dir}/cppcheck_suppression.txt")
+endmacro()
 
-# option(USE_CPPCHECK "Run cppcheck on the source files" OFF)
-# if(USE_CPPCHECK)
-#     find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-#     if(NOT CMAKE_CXX_CPPCHECK)
-#         message(FATAL_ERROR "can't find cppcheck")
-#     endif()
-
-#     set(cppcheck_flag 
-#         "--enable=all"
-#         "-q"
-#         "--force"
-#         "--suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck_suppression.txt")
-
-#     cmake_parse_arguments(PARSED "" "EXTRA_FLAG;OVERRIDE_FLAG" "" ${ARGN})
-
-#     if((PARSED_EXTRA_FLAG) AND (PARSED_OVERRIDE_FLAG))
-#         message(FATAL_ERROR "EXTRA_FLAG and OVERRIDE_FLAG can't be defined at the same time")
-#     endif()
-#     if(PARSED_EXTRA_FLAG)
-#         string(APPEND cppcheck_flag " " ${PARSED_EXTRA_FLAG})
-#     endif()
-#     if(PARSED_OVERRIDE_FLAG)
-#         set(cppcheck_flag ${PARSED_OVERRIDE_FLAG})
-#     endif()
-
-#     list(APPEND CMAKE_CXX_CPPCHECK ${cppcheck_flag})
-# endif()
+# has 2 optional args
+# util_add_cpplint EXTRA_FLAG extra_flag OVERRIDE_FLAG override_flag
+macro(util_add_cpplint)
+    util_add_program_base(cpplint 
+                          OFF 
+                          CMAKE_CXX_CPPLINT
+                          "--filter=-legal/copyright, -whitespace/line_length, -whitespace/ending_newline, -build/c++11, -runtime/references, -whitespace/indent"
+                          "--quiet")
 endmacro()
