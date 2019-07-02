8f516531565937580e948b31e51ff7354778f22c
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-02 13:15:01 -0700

    fix: Fixed optional arg

--- utils_tool.cmake
+++ utils_tool.cmake
@@ -1,26 +1,32 @@
-macro(util_add_cppcheck extra_flag override_flag)
+# has 2 optional args
+# util_add_cppcheck EXTRA_FLAG extra_flag OVERRIDE_FLAG override_flag
+function(util_add_cppcheck)
 option(USE_CPPCHECK "Run cppcheck on the source files" OFF)
 if(USE_CPPCHECK)
-  find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-  if(NOT CMAKE_CXX_CPPCHECK)
-    message(FATAL_ERROR "can't find cppcheck")
-  endif()
+    find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
+    if(NOT CMAKE_CXX_CPPCHECK)
+        message(FATAL_ERROR "can't find cppcheck")
+    endif()
 
-  set(cppcheck_flag 
+    set(cppcheck_flag 
         "--enable=all"
         "-q"
         "--force"
         "--suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck_suppression.txt")
 
-  if(${ARGC} EQUAL 1)
-  string(APPEND cppcheck_flag " " ${extra_flag})
-  endif()
-  if(${ARGC} EQUAL 2)
-  set(cppcheck_flag ${override_flag})
-  endif()
+    set(oneValueArgs EXTRA_FLAG OVERRIDE_FLAG)
+    cmake_parse_arguments(util_add_cppcheck "${oneValueArgs}")
 
-  list(
-    APPEND CMAKE_CXX_CPPCHECK
-           ${cppcheck_flag})
+    if(EXTRA_FLAG AND OVERRIDE_FLAG)
+        message(FATAL_ERROR "EXTRA_FLAG and OVERRIDE_FLAG can't be defined at the same time")
+    endif()
+    if(EXTRA_FLAG)
+        string(APPEND cppcheck_flag " " ${EXTRA_FLAG})
+    endif()
+    if(OVERRIDE_FLAG)
+        set(cppcheck_flag ${OVERRIDE_FLAG})
+    endif()
+
+    list(APPEND CMAKE_CXX_CPPCHECK ${cppcheck_flag})
 endif()
-endmacro()
+endfunction()
