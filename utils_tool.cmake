48eb595da253bbbadcdf3667f9913e843f890fd7
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-03 00:22:37 -0700

    feat: Added code gen option

--- utils_tool.cmake
+++ utils_tool.cmake
@@ -7,7 +7,7 @@ macro(util_add_program_base program_name default_switch append_var)
             message(FATAL_ERROR "can't find ${program_name}")
         endif()
 
-        cmake_parse_arguments(${program_name}_PARSED "" "EXTRA_FLAG;OVERRIDE_FLAG" "" ${ARGN})
+        cmake_parse_arguments(${program_name}_PARSED "" "" "EXTRA_FLAG;OVERRIDE_FLAG" ${ARGN})
         
         if((${program_name}_PARSED_EXTRA_FLAG) AND (${program_name}_PARSED_OVERRIDE_FLAG))
             message(FATAL_ERROR "EXTRA_FLAG and OVERRIDE_FLAG can't be defined at the same time")
@@ -15,7 +15,7 @@ macro(util_add_program_base program_name default_switch append_var)
 
         set(${program_name}_program_flag ${ARGN})
         if(${program_name}_PARSED_EXTRA_FLAG)
-            string(APPEND ${program_name}_program_flag " " ${${program_name}_PARSED_EXTRA_FLAG})
+            list(APPEND ${program_name}_program_flag ${${program_name}_PARSED_EXTRA_FLAG})
         endif()
         if(${program_name}_PARSED_OVERRIDE_FLAG)
             set(${program_name}_program_flag ${${program_name}_PARSED_OVERRIDE_FLAG})
@@ -51,4 +51,68 @@ macro(util_check_and_enable_test)
     if(RUN_TEST)
         enable_testing()
     endif()
+endmacro()
+
+# util_add_general_code_gen_option(CPP_STANDARD cpp_std EXTRA_COMPILE_FLAG flag1 flag2 EXTRA_LINK_FLAG flag1 flag2)
+macro(util_add_general_code_gen_option)
+    cmake_parse_arguments(CODE_GEN_PARSED "" "CPP_STANDARD" "EXTRA_COMPILE_FLAG;EXTRA_LINK_FLAG" ${ARGN})
+
+    if(CODE_GEN_PARSED_CPP_STANDARD)
+        set(CMAKE_CXX_STANDARD ${CODE_GEN_PARSED_CPP_STANDARD})
+    else()
+        set(CMAKE_CXX_STANDARD 17)
+    endif()
+
+    set(util_compile_flag -Wall
+                          -Wextra
+                          -Werror
+                          -Wnon-virtual-dtor
+                          -Wold-style-cast
+                          -Woverloaded-virtual
+                          -Wsign-conversion
+                          -Wconversion
+                          -Wduplicated-cond
+                          -Wduplicated-branches
+                          -Wlogical-op
+                          -Wnull-dereference
+                          -Wuseless-cast
+                          -Wformat=2
+                          -Wdouble-promotion
+                          -fasynchronous-unwind-tables
+                          -fstack-protector
+                          -fstack-protector-strong
+                          -fPIC
+                          -shared
+                          -pipe
+                          -fsanitize=address
+                          -fsanitize=undefined
+                          -g
+                          -fdiagnostics-color=always
+                          -fno-omit-frame-pointer)
+    if(CODE_GEN_PARSED_EXTRA_COMPILE_FLAG)
+        list(APPEND util_compile_flag ${CODE_GEN_PARSED_EXTRA_COMPILE_FLAG})
+    endif()
+    add_compile_options(${util_compile_flag})
+
+    set(util_link_flag -fsanitize=address -fsanitize=undefined)
+    if(CODE_GEN_PARSED_EXTRA_LINK_FLAG)
+        list(APPEND util_link_flag ${CODE_GEN_PARSED_EXTRA_LINK_FLAG})
+    endif()
+    add_link_options(${util_link_flag})
+endmacro()
+
+# util_set_thirdparty_code_gen_option(target1 target2)
+macro(util_set_thirdparty_code_gen_option)
+    foreach(arg IN ITEMS ${ARGN})
+        set_target_properties(${arg} PROPERTIES CMAKE_CXX_STANDARD 17)
+        set_property(
+        TARGET ${arg}
+        PROPERTY COMPILE_OPTIONS -g 
+                                 -fsanitize=address
+                                 -fsanitize=undefined)
+        set_property(
+        TARGET ${arg}
+        PROPERTY LINK_OPTIONS -fsanitize=address 
+                              -fsanitize=undefined)
+    endforeach()
 endmacro()
